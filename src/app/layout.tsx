25f13bf4cb22feca0d4a32a5b6a6dbd6ff1d08a1
Yash Ramesh Kumar <devyk100@gmail.com>
2025-01-05 10:54:56 +0530

    cleaned up more code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Montserrat } from "next/font/google";
+import { Geist_Mono, Montserrat } from "next/font/google";
 import "./globals.css";
 import { NavigationMenuDemo } from "@/components/custom-navbar";
 import { ThemeProvider } from "@/components/providers/theme-provider"
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/components/ui/button";
-import { ModeToggle } from "@/components/ui/dark-mode-toggle";
 
 export default function Home() {
   return (
