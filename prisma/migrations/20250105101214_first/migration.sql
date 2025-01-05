f8388e2e3edc95386ef3cd8325f09d6e90244479
Yash Ramesh Kumar <devyk100@gmail.com>
2025-01-05 15:58:14 +0530

    some changes to the DB, always migrate --create-only and check the file yourself and only then deploy

---
+++ prisma/migrations/20250105101214_first/migration.sql
@@ -0,0 +1,19 @@
+-- -- DropEnum
+-- DROP TYPE "crdb_internal_region";
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" STRING NOT NULL,
+    "username" STRING NOT NULL,
+    "password" STRING NOT NULL,
+    "name" STRING NOT NULL,
+    "email" STRING NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
