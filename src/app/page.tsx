126978d6bbef66bdeb428e04ccf6945f834a357b
Yash Ramesh Kumar <devyk100@gmail.com>
2025-01-06 23:26:59 +0530

    important for dynamic rendering:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/prisma";
+
+// IMPORTANT
+export const dynamic = 'force-dynamic'; 
+
 export default async function Home() {
   const resp = await prisma.user.findMany({});
   console.log(resp)
